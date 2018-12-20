1c29570a9877f56bf5475af5184d61738abecf6c
Pawel Bachta <bachta.pawel@gmail.com>
2018-12-20 17:42:28 +0100

    add new dependences to pom

--- src/main/java/com/example/appdemo/AppdemoApplication.java
+++ src/main/java/com/example/appdemo/AppdemoApplication.java
@@ -2,8 +2,10 @@ package com.example.appdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class AppdemoApplication {
 
 	public static void main(String[] args) {
