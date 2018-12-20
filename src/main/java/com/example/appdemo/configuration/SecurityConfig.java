721d18305802f8a6cfffc83bbb2775353898b0d4
Pawel Bachta <bachta.pawel@gmail.com>
2018-12-20 17:44:27 +0100

    add servletinitializer and spring security configuration

---
+++ src/main/java/com/example/appdemo/configuration/SecurityConfig.java
@@ -0,0 +1,63 @@
+package com.example.appdemo.configuration;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Value("${spring.queries.users-query}")
+    private String userQuery;
+
+    @Value("${spring.queries.roles-query}")
+    private String rolesQuery;
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().authoritiesByUsernameQuery(userQuery)
+                .authoritiesByUsernameQuery(rolesQuery)
+                .dataSource(dataSource).passwordEncoder(bCryptPasswordEncoder);
+    }
+
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/login").permitAll()
+                .antMatchers("/register").permitAll()
+                .antMatchers("/adduser").permitAll()
+                .antMatchers("/admin").hasAuthority("ROLE_ADMIN")
+                .anyRequest().authenticated()
+                .and().csrf().disable()
+                .formLogin()
+                .loginPage("/login")
+                .failureUrl("/login?error=true")
+                .defaultSuccessUrl("/").usernameParameter("email")
+                .passwordParameter("password")
+                .and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/")
+                .and().exceptionHandling().accessDeniedPage("/denied");
+    }
+
+    public void configure(WebSecurity webSecurity) throws Exception {
+        webSecurity.ignoring()
+                .antMatchers("/resources/**" , "/statics/**", "/css/**", "/js/**", "/images/**", "/incl/**");
+    }
+
+}
