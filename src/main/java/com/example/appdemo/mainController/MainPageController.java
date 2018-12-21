f22ced0ee8eda53e0afb0ac4d2eda61b77e9fe96
Pawel Bachta <bachta.pawel@gmail.com>
2018-12-21 16:38:00 +0100

    small change in main page controller

--- src/main/java/com/example/appdemo/mainController/MainPageController.java
+++ src/main/java/com/example/appdemo/mainController/MainPageController.java
@@ -3,18 +3,14 @@ package com.example.appdemo.mainController;
 import javax.ws.rs.GET;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-//@RestController
 @Controller
 public class MainPageController {
 
     @GET
     @RequestMapping(value = {"/", "/index"})
     public String showMainPage() {
-        return "index1";
+        return "index";
     }
 }
