994ab1451be01c468c6f038d0628de48e76273d2
Pawel Bachta <bachta.pawel@gmail.com>
2019-01-07 16:32:03 +0100

    add error page

---
+++ src/main/java/com/example/appdemo/mainController/ErrorPageController.java
@@ -0,0 +1,22 @@
+package com.example.appdemo.mainController;
+
+import javax.ws.rs.GET;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ErrorPageController implements ErrorController {
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+
+    @GET
+    @RequestMapping(value = "/error")
+    public String showErrorPage() {
+        return "error";
+    }
+}
