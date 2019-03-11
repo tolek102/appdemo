6f246e13e2858f0e591afc578511eb5fd04d9a5b
Pawel Bachta <bachta.pawel@gmail.com>
2019-03-11 22:38:13 +0100

    add secure control with @Secured adnotation

---
+++ src/main/java/com/example/appdemo/admin/AdminPageController.java
@@ -0,0 +1,18 @@
+package com.example.appdemo.admin;
+
+import javax.ws.rs.GET;
+
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AdminPageController {
+
+    @GET
+    @RequestMapping(value = "/admin")
+    @Secured(value = {"ROLE_ADMIN"})
+    public String openAdminPage() {
+        return "admin/admin";
+    }
+}
--- src/main/java/com/example/appdemo/configuration/SecurityConfig.java
+++ src/main/java/com/example/appdemo/configuration/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,6 +16,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true) // włącza obsługę adnotacji @Secured
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -42,7 +44,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         httpSecurity
                 .authorizeRequests()
                 .antMatchers(SecurityParam.PERMITTED_TO_ALL).permitAll()
-                .antMatchers(SecurityParam.PERMITTED_ONLY_TO_ADMIN).hasAuthority(SecurityParam.ROLE_ADMIN)
+//                .antMatchers(SecurityParam.PERMITTED_ONLY_TO_ADMIN).hasAuthority(SecurityParam.ROLE_ADMIN)
                 .anyRequest().authenticated()
                 .and().csrf().disable()
                 .formLogin()
